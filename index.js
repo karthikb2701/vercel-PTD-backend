1308a3208abefae8d64bcc1964be50cd62c7e9d1
Karthikb2701@ <karthikb2701@gmail.com>
2025-06-21 12:45:15 +0530

    adadd 2

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(cookieParser())
 // for allowing json object in req body
 app.use(express.json())
 
-app.listen("https://vercel-ptd-backend-fzm6.vercel.app/", () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000!")
 })
 
